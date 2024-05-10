c7982346c5cd909920e5c67509a496fc840468db
User_4V9 <a.21308051280516@cbtis128.edu.mx>
2024-05-10 06:55:37 +0000

    actualizar

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,93 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class MyApp extends StatefulWidget {
+// This widget is the root of your application.
+  @override
+  MyAppState createState() => MyAppState();
+}
+
+class MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Scaffold Demo',
       theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
-      ),
-    );
+        primarySwatch: Colors.pink,
+      ), // ThemeData
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('Pandora'),
+          ), // AppBar
+          body: Container(
+            alignment: Alignment.center,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    print('Brazaletes');
+                  },
+                  child: Text('Brazaletes'),
+                  style: ElevatedButton.styleFrom(
+                    onPrimary: Colors.white,
+                    primary: Color(0xffff9898),
+                    textStyle: TextStyle(
+                      fontSize: 36,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                OutlinedButton(
+                  onPressed: () {
+                    print('Anillos');
+                  },
+                  child: Text('Anillos'),
+                  style: ButtonStyle(
+                    foregroundColor:
+                        MaterialStateProperty.all<Color>(Color(0xff000000)),
+                    backgroundColor: MaterialStateProperty.resolveWith<Color>(
+                        (Set<MaterialState> states) {
+                      if (states.contains(MaterialState.pressed)) {
+                        return Color(
+                            0xffff7989); //when button pressed, return blue
+                      }
+                      return Color(0xffbdfff3);
+                    }),
+                    textStyle: MaterialStateProperty.resolveWith<TextStyle>(
+                        (Set<MaterialState> states) {
+                      if (states.contains(MaterialState.pressed)) {
+                        return TextStyle(
+                            fontSize: 36,
+                            fontWeight: FontWeight.bold,
+                            fontStyle: FontStyle.italic);
+                      }
+                      return TextStyle();
+                    }),
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {
+                    print('Collares');
+                  },
+                  child: Text('Collares'),
+                  style: TextButton.styleFrom(
+                    primary: Colors.black,
+                    textStyle: TextStyle(
+                      fontSize: 36,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          )),
+    ); // Scaffold ); // MaterialApp
   }
 }
